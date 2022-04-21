e0646b84406e4990bf472435a789057e072052b7
unknown <mairurated@gmail.com>
2022-04-21 15:17:28 +0300

    cart

---
+++ src/data.js
@@ -0,0 +1,31 @@
+import img1 from "./img/img1.avif";
+import img2 from "./img/img2.jpg";
+import img3 from "./img/img3.avif";
+
+const data = {
+  productData: [
+    {
+      id: 1,
+      img: img1,
+      title: "Nike Shoes",
+      desc: "High Quality",
+      price: "1200",
+    },
+    {
+      id: 2,
+      img: img2,
+      title: "Classy Girls",
+      desc: "High Quality",
+      price: "500",
+    },
+    {
+      id: 3,
+      img: img3,
+      title: "Telephone",
+      desc: "",
+      price: "1200",
+    },
+  ],
+};
+
+export default data;
